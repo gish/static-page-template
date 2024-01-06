2eb28ebcea3a1598bdcb9063519c0ac87475af3f
Erik Hedberg <erik@hedberg.at>
2024-01-06 17:19:32 +0100

    add more variables

--- terraform.tf
+++ terraform.tf
@@ -13,7 +13,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "hedberg-terraform-states"
-    key    = var.domain_name
+    key    = "" // Change this
     region = "eu-north-1"
   }
 }
